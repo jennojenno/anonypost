6c8a7882997df5e93972534dabee92cd2c80b28d
Jen Lim <jennolim@gmail.com>
2014-03-20 11:46:00 -0400

    Start of new Anonypost app, rails files only

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anonypost::Application.config.secret_key_base = '6b96aae9363a7bb6e4d76f38fe7a8f51ece3c56032cca70fb96fcdf72858b931df2ce27e72a652ceacd4a7943230e36470102073d620788ae1e76bc9e73e8a42'
